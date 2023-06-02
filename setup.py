fd50a8fe36f0c5c0c55790688345688a4937b444
Pratik Anand <13189088+pratik141@users.noreply.github.com>
2023-06-02 15:53:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nsedt",
-    version="0.0.2",
+    version="0.0.3",
     author="Pratik Anand",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
