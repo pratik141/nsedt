23fda0e1dd43137df50dfe6b21ca824718e68e62
Pratik Anand <13189088+pratik141@users.noreply.github.com>
2024-05-05 11:15:53 +0200

    Doc Update

--- setup.py
+++ setup.py
@@ -8,27 +8,26 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required_lib = f.read().splitlines()
+
 setup(
     name="nsedt",
     version=os.getenv("GITHUB_REF_NAME"),
     author="Pratik Anand",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    description="Library to collect NSE data in pandas dataframe",
+    description="Library to collect NSE data",
     packages=find_packages(),
     url="https://github.com/pratik141/nsedt",
-    install_requires=[
-        "requests",
-        "numpy",
-        "pandas",
-        "fake-http-header"
-    ],
+    install_requires=required_lib,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
